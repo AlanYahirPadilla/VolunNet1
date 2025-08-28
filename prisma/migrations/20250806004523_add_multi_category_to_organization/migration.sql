fdd5df3e84429a988a3ed956d6ea65bca73864b4
AlanYahirPadilla <yahirpadilla94@gmail.com>
2025-08-27 21:06:38 -0600

    Eliminando todos los archivos

--- prisma/migrations/20250806004523_add_multi_category_to_organization/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `category` column on the `organizations` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "organizations" DROP COLUMN "category",
-ADD COLUMN     "category" TEXT[];
